a60b3eb35b3a66f91326fd04271451267d69b155
bradferd <bbailey921@gmail.com>
2020-05-21 11:48:16 -0400

    Edit for SlidingWindow problem

--- SlidingWindow.js
+++ SlidingWindow.js
@@ -4,24 +4,18 @@
 // in the array.
 
 function maxSubarraySum(arr, num) {
-    if (num > arr.length) {
-        return null;
+    let maxSum = 0;
+    let tempSum = 0;
+    if (arr.length < num) return null;
+    for (let i = 0; i < num; i++) {
+        maxSum += arr[i];
     }
-
-    var max = -Infinity;
-
-    for (let i = 0; i < arr.length - num + 1; i++) {
-        temp = 0;
-        for (let j = 0; j < num; j++) {
-            temp += arr[i + j];
-        }
-        if (temp > max) {
-            max = temp;
-        }
-        console.log(temp, max);
+    tempSum = maxSum;
+    for (let i = num; i < arr.length; i++) {
+        tempSUm = tempSum - arr[i - num] + arr[i];
+        maxSum = Math.max(maxSum, tempSum);
     }
-    
-    return max;
+    return maxSum;
 }
 
 maxSubarraySum([2,6,9,2,1,8,5,6,3], 3);
